da66661b8d2eb36eb0d7f5c5560f12d80a81c18d
phsmartins <phsmartins2004@gmail.com>
2024-12-01 19:55:04 -0400

    modifica frase principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá, Mundo!</h1>
+    return <h1>Hoje não foi um dia tão fácil e tão pouco amanhã será, mas você é capaz e tenha força para passar por isso. Eu te amo</h1>
 }
 
 export default Home;
