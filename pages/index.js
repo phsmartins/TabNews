89ae3bf8c1cc97fa5071c19bd39bd411aa70bcda
Pedro Martins <99466009+phsmartins@users.noreply.github.com>
2024-12-03 16:07:56 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Hoje não foi um dia tão fácil e tão pouco amanhã será, mas você é capaz e tenha força para passar por isso. Eu te amo</h1>
+  return (
+    <h1>
+      Hoje não foi um dia tão fácil e tão pouco amanhã será, mas você é capaz e
+      tenha força para passar por isso. Eu te amo
+    </h1>
+  );
 }
 
 export default Home;
