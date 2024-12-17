1488a6a02b73189b3ada1cebb9dda74d326488d5
Pedro Martins <99466009+phsmartins@users.noreply.github.com>
2024-12-17 21:11:00 +0000

    endpoint status: max_connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,14 +2,19 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const resultDatabaseVersion = await database.query("SHOW server_version");
-  const databaseVersion = resultDatabaseVersion.rows[0].server_version;
+
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersion,
+        max_connections: parseInt(maxConnections),
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,17 +1,11 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
   const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  const responseUpdatedAt = responseBody.updated_at;
-  const parsedUpdatedAt = new Date(responseUpdatedAt).toISOString();
-
-  const responseDatabaseVersion = responseBody.dependencies.database.version;
-
-  expect(responseUpdatedAt).toBeDefined();
-  expect(responseUpdatedAt).toEqual(parsedUpdatedAt);
-
-  expect(responseDatabaseVersion).toBeDefined();
-  expect(responseDatabaseVersion).toEqual("16.0");
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
   expect(response.status).toBe(200);
 });
