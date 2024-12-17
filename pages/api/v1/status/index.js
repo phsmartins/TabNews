659ed9e2ca3bd83abf2652c7ce7956979ea7a1cf
Pedro Martins <99466009+phsmartins@users.noreply.github.com>
2024-12-17 20:43:55 +0000

    /status: database version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,18 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const resultDatabaseVersion = await database.query("SHOW server_version");
+  const databaseVersion = resultDatabaseVersion.rows[0].server_version;
 
-  response.status(200).json({ chave: "Hello World" });
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  const responseUpdatedAt = responseBody.updated_at;
+  const parsedUpdatedAt = new Date(responseUpdatedAt).toISOString();
+
+  const responseDatabaseVersion = responseBody.dependencies.database.version;
+
+  expect(responseUpdatedAt).toBeDefined();
+  expect(responseUpdatedAt).toEqual(parsedUpdatedAt);
+
+  expect(responseDatabaseVersion).toBeDefined();
+  expect(responseDatabaseVersion).toEqual("16.0");
+
   expect(response.status).toBe(200);
 });
