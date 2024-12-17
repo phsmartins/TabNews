39ad7a75249c4bb6a33165a0b177281c267d7cdd
Pedro Martins <99466009+phsmartins@users.noreply.github.com>
2024-12-17 21:54:55 +0000

    endpoint status: opened_connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,15 +6,25 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version");
   const databaseVersion = databaseVersionResult.rows[0].server_version;
 
-  const maxConnectionsResult = await database.query("SHOW max_connections");
-  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'postgres';",
+  );
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersion,
-        max_connections: parseInt(maxConnections),
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,6 +6,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 
   expect(response.status).toBe(200);
 });
